d16f6e526ad591567ad717d7e638b9da3871bf98
Aswankappingal <instamagic774@gmail.com>
2024-01-26 22:25:27 +0530

    pppp

--- watch-project/src/Components/Customer/CartCutomer/Cartcustomer.jsx
+++ watch-project/src/Components/Customer/CartCutomer/Cartcustomer.jsx
@@ -176,7 +176,7 @@ const Cartcustomer = () => {
               <td>₹ 99</td>
             </tr>
           </table>
-          <div className="table-ul "></div>
+          <div className="table-ul www"></div>
           <table>
             <tr>
               <td className='total-text' id='td'>Total</td>
